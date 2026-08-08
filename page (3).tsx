85cbedaff38dfce662d15427430d470d303df8ed
plut07 <kinhou20@gmail.com>
2026-08-08 10:48:49 +0800

    1.1

--- page (3).tsx
+++
@@ -1,9 +0,0 @@
-"use server";
-
-import { redirect } from "next/navigation";
-import { clearSessionCookie } from "@/lib/session";
-
-export async function logoutAction() {
-  clearSessionCookie();
-  redirect("/login");
-}
